23e0a296600bf280845fcc4bf614e1d36ecb452d
ozkanpakdil <ozkan.pakdil@gmail.com>
2021-05-10 22:22:21 +0100

    exe test

--- runtests.sh
+++ runtests.sh
@@ -5,10 +5,10 @@ TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars jtwig s
 > result-$1.txt
 
 mvn -version
-mvn spring-boot:run &
-sleep 7
-
+mvn package
+java --add-opens java.base/java.lang=ALL-UNNAMED -jar ./target/template-engines.war > log.log & 
 JPID=$!
+sleep 7
 
 for ip in "${TESTS[@]}"; do
   result=`ab -q -n 1000 -c 10 http://localhost:8080/$ip | grep "Time taken for tests"`
