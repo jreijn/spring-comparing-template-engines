4f9b70ce5483e88d29b7f2f80d2bf02d24234da5
ozkanpakdil <ozkan.pakdil@gmail.com>
2021-05-10 20:18:35 +0100

    exec test

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -x
+set -x
 JVMVER=$1
 TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars jtwig scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp kotlinx)
 > result-$1.txt
@@ -9,7 +9,7 @@ JPID=$!
 
 for ip in "${TESTS[@]}"; do
   result=`ab -q -n 1000 -c 10 http://localhost:8080/$ip | grep "Time taken for tests"`
-  echo $ip $result >> result-$1.txt
+  echo "$ip $result" >> result-$1.txt
 done
 kill -9 $JPID
 git pull
