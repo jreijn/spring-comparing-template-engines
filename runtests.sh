873b6742c034b4b5a2578c1bd41df07cf976279a
ozkanpakdil <ozkan.pakdil@gmail.com>
2021-05-10 21:15:52 +0100

    exec test

--- runtests.sh
+++ runtests.sh
@@ -4,10 +4,8 @@ JVMVER=$1
 TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars jtwig scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp kotlinx)
 > result-$1.txt
 
-java --add-opens java.base/java.lang=ALL-UNNAMED -jar ./target/template-engines.war # > log.log & 
+mvn spring-boot:run > log.log & 
 JPID=$!
-
-echo PID:$JPID
 
 for ip in "${TESTS[@]}"; do
   result=`ab -q -n 1000 -c 10 http://localhost:8080/$ip | grep "Time taken for tests"`
