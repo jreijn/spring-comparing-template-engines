73db7250990003c81fe2bc13146932f26d24ceff
ozkanpakdil <ozkan.pakdil@gmail.com>
2021-05-15 08:23:04 +0100

    clean up

--- runtests.sh
+++ runtests.sh
@@ -12,9 +12,9 @@ mvn -B spring-boot:run &
 JPID=$!
 sleep 80 # waiting for spring boot to start
 
-for ip in "${TESTS[@]}"; do
-  result=`ab -q -n 1000 -c 10 http://localhost:8080/$ip | grep "Time taken for tests"`
-  echo "$ip $result" >> result-$1.txt
+for template in "${TESTS[@]}"; do
+  result=`ab -q -n 1000 -c 10 http://localhost:8080/$template | grep "Time taken for tests"`
+  echo "$template $result" >> result-$1.txt
 done
 kill -9 $JPID
 git config pull.rebase true
@@ -24,13 +24,19 @@ sleep $[ ( $JVMVER % 20 ) + 1 ]s
 sonuc=`cat result-$1.txt`
 date=`date`
 
-#git checkout gh-pages
-#git switch gh-pages
 cat > index.md <<EOL
+
+     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+     <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+               google_ad_client: "ca-pub-7118095690658891",
+               enable_page_level_ads: true
+          });
+     </script>
 
 ## Spring template engine performance tests
 
-Runs performance test from [Github Actions](https://github.com/ozkanpakdil/spring-comparing-template-engines/actions) and updates here.
+Runs performance test(ab -q -n 1000 -c 10 http://localhost:8080/$template | grep "Time taken for tests") from [Github Actions](https://github.com/ozkanpakdil/spring-comparing-template-engines/actions) and updates here.
 
 ### Results from $date
 
@@ -39,10 +45,4 @@ $sonuc
 \`\`\`
 
 EOL
-
-#git add .
-#git commit -m "updating the page"
-#git push
-#git switch master
-#git push
 exit 0
