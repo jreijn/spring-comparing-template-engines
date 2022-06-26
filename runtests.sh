fe75588d520b5a25d9fa92f948cf06b5d4faf60b
Vest <Vest@users.noreply.github.com>
2022-06-26 16:46:30 +0200

    Tested and implemented PR #66 - kotlin is now the latest Fixed the issue with Spring default Servlet Removed jtwig, because this project was abandoned by the maintainers. Ignore some temporary folders
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 ADRESS=$1
-TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars jtwig scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp)
+TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp)
 
 for ip in "${TESTS[@]}"; do
   result=`ab -q -n 1000 -c 10 http://$ADRESS:8080/$ip | grep "Time taken for tests"`
