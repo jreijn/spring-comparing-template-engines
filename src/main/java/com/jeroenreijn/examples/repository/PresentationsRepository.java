14e64389813f5c39fb12fbf9a686f65041a8ba63
Vest <Vest@users.noreply.github.com>
2023-02-23 11:53:38 +0100

    Updated HtmlFlow from 3.5 to 4.0 (#96)
    * Removed unused method from the InMemoryPresentationsRepository
    * Increased the package version to 0.9 due to major changes in the code.

--- src/main/java/com/jeroenreijn/examples/repository/PresentationsRepository.java
+++ src/main/java/com/jeroenreijn/examples/repository/PresentationsRepository.java
@@ -1,11 +1,7 @@
 package com.jeroenreijn.examples.repository;
 
-import java.util.Optional;
-
 import com.jeroenreijn.examples.model.Presentation;
 
 public interface PresentationsRepository {
 	Iterable<Presentation> findAll();
-
-	Optional<Presentation> findById(Long id);
 }
--- src/main/java/com/jeroenreijn/examples/view/HtmlFlowView.java
+++ src/main/java/com/jeroenreijn/examples/view/HtmlFlowView.java
@@ -1,11 +1,14 @@
 package com.jeroenreijn.examples.view;
 
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.jeroenreijn.examples.model.Presentation;
 import org.springframework.web.servlet.view.AbstractTemplateView;
 
 public class HtmlFlowView extends AbstractTemplateView {
@@ -13,7 +16,8 @@ public class HtmlFlowView extends AbstractTemplateView {
 	@Override
 	protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		String html = HtmlFlowIndexView.view.render(model);
+		Stream<Presentation> presentations = ((List<Presentation>) model.get("presentations")).stream();
+		String html = HtmlFlowIndexView.view.render(presentations);
 
 		response.setContentLength(html.length());
 		response.setContentType("text/html");
