1ad7f2e559a28fa0f3eaac4850d7b1b0269a7347
Adam Gent <adam.gent@snaphop.com>
2023-12-15 09:52:15 -0500

    Add jstachio to shell script

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 ADDRESS=$1
-TESTS=(jsp velocity freemarker thymeleaf mustache jade pebble handlebars scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp)
+TESTS=(jsp jstachio velocity freemarker thymeleaf mustache jade pebble handlebars scalate httl chunk htmlFlow trimou rocker ickenham rythm groovy liqp)
 
 for ip in "${TESTS[@]}"; do
   result=`ab -q -n 1000 -c 10 http://$ADDRESS:8080/$ip | grep "Time taken for tests"`
